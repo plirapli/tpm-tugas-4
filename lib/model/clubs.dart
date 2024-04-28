d44a726bf16c359731b0d268a9ae5cab473ac20a
plirapli <rafliyulistiawan@gmail.com>
2024-04-28 13:56:52 +0700

    add model for data

---
+++ lib/model/clubs.dart
@@ -0,0 +1,146 @@
+class Clubs {
+  int? id;
+  String? name;
+  String? code;
+  String? logo;
+  String? url;
+
+  Clubs({this.id, this.name, this.code, this.logo, this.url});
+}
+
+List<Clubs> clubList = [
+  Clubs(
+      id: 1,
+      name: "Burnley FC",
+      code: "BUR",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/6/6d/Burnley_FC_Logo.svg",
+      url: "https://www.burnleyfootballclub.com/"),
+  Clubs(
+      id: 2,
+      name: "Manchester United FC",
+      code: "MUN",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/9/9f/Aston_Villa_logo.svg",
+      url: "https://www.manutd.com/"),
+  Clubs(
+      id: 3,
+      name: "Manchester City FC",
+      code: "MCI",
+      logo: "",
+      url: "https://www.mancity.com/"),
+  Clubs(
+      id: 4,
+      name: "Aston Villa FC",
+      code: "AVL",
+      logo:
+          "https://en.wikipedia.org/wiki/Aston_Villa_F.C.#/media/File:Aston_Villa_logo.svg",
+      url: "https://www.avfc.co.uk/"),
+  Clubs(
+      id: 5,
+      name: "Fulham FC",
+      code: "FUL",
+      logo: "https://upload.wikimedia.org/wikipedia/id/1/13/Fulham_FC.svg",
+      url: "https://www.fulhamfc.com/"),
+  Clubs(
+      id: 6,
+      name: "Arsenal FC",
+      code: "ARS",
+      logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
+      url: "https://www.arsenal.com/"),
+  Clubs(
+      id: 7,
+      name: "Crystal Palace FC",
+      code: "CRY",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/a/a2/Crystal_Palace_FC_logo_%282022%29.svg",
+      url: "https://www.cpfc.co.uk/"),
+  Clubs(
+      id: 8,
+      name: "Southampton FC",
+      code: "SOU",
+      logo: "https://upload.wikimedia.org/wikipedia/en/c/c9/FC_Southampton.svg",
+      url: "https://www.southamptonfc.com/en"),
+  Clubs(
+      id: 9,
+      name: "Liverpool FC",
+      code: "LIV",
+      logo: "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
+      url: "https://www.liverpoolfc.com/"),
+  Clubs(
+      id: 10,
+      name: "Brentford",
+      code: "BRE",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/2/2a/Brentford_FC_crest.svg",
+      url: "https://www.brentfordfc.com/en"),
+  Clubs(
+      id: 11,
+      name: "West Ham United FC",
+      code: "WHU",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/c/c2/West_Ham_United_FC_logo.svg",
+      url: "https://www.whufc.com/"),
+  Clubs(
+      id: 12,
+      name: "Newcastle United FC",
+      code: "NEW",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/5/56/Newcastle_United_Logo.svg",
+      url: "https://www.newcastleunited.com/"),
+  Clubs(
+      id: 13,
+      name: "Luton Town FC",
+      code: "LUT",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/9/9d/Luton_Town_logo.svg",
+      url: "https://www.lutontown.co.uk/"),
+  Clubs(
+      id: 14,
+      name: "AFC Bournemouth",
+      code: "BOU",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/e/e5/AFC_Bournemouth_%282013%29.svg",
+      url: "https://www.afcb.co.uk/"),
+  Clubs(
+      id: 15,
+      name: "Tottenham Hotspur FC",
+      code: "TOT",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/b/b4/Tottenham_Hotspur.svg",
+      url: "https://www.tottenhamhotspur.com/"),
+  Clubs(
+      id: 16,
+      name: "Everton FC",
+      code: "EVE",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/7/7c/Everton_FC_logo.svg",
+      url: "https://www.evertonfc.com/"),
+  Clubs(
+      id: 17,
+      name: "Sheffield United FC",
+      code: "SHU",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/9/9c/Sheffield_United_FC_logo.svg",
+      url: "https://www.sufc.co.uk/"),
+  Clubs(
+      id: 18,
+      name: "Wolverhampton Wanderers FC",
+      code: "WOL",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/f/fc/Wolverhampton_Wanderers.svg",
+      url: "https://www.wolves.co.uk/"),
+  Clubs(
+      id: 19,
+      name: "Brighton & Hove Albion FC",
+      code: "BHA",
+      logo:
+          "https://upload.wikimedia.org/wikipedia/en/d/d0/Brighton_and_Hove_Albion_FC_crest.svg",
+      url: "https://www.brightonandhovealbion.com/"),
+  Clubs(
+      id: 20,
+      name: "Chelsea FC",
+      code: "CHE",
+      logo: "https://upload.wikimedia.org/wikipedia/en/c/cc/Chelsea_FC.svg",
+      url: "https://www.chelseafc.com/en")
+];
