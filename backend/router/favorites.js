576846ff5aaee902f84e2e770b9c77383ddef336
plirapli <rafliyulistiawan@gmail.com>
2024-05-12 22:27:08 +0700

    add new endpoint for delete fav clubs

--- backend/router/favorites.js
+++ backend/router/favorites.js
@@ -35,7 +35,7 @@ router.post("/:user_id", async (req, res, next) => {
     const { club_id } = req.body;
 
     if (!user_id || !club_id) {
-      const error = new Error("Cannot be empty.");
+      const error = new Error("Field Cannot be empty.");
       error.statusCode = 401;
       throw error;
     }
@@ -62,6 +62,49 @@ router.post("/:user_id", async (req, res, next) => {
     res.status(201).json({
       status: "Success",
       message: "Added to favorites.",
+    });
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+});
+
+// [DELETE] Delete favorite club by user_id
+router.delete("/:user_id", async (req, res, next) => {
+  try {
+    const { user_id } = req.params;
+    const { club_id } = req.body;
+
+    if (!user_id || !club_id) {
+      const error = new Error("Cannot be empty.");
+      error.statusCode = 401;
+      throw error;
+    }
+    if (club_id <= 0 || club_id > 20) {
+      const error = new Error("Club not found.");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const checkQuery = `SELECT * FROM favorites WHERE user_id = ? AND club_id = ?`;
+    const [favorites] = await connection
+      .promise()
+      .query(checkQuery, [user_id, club_id]);
+
+    if (favorites.length == 0) {
+      const error = new Error("Club doesn't exist in favorites.");
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const query = "DELETE FROM favorites WHERE user_id = ? AND club_id = ?";
+    await connection.promise().query(query, [user_id, club_id]);
+
+    res.status(200).json({
+      status: "Success",
+      message: "Deleted from favorites.",
     });
   } catch (error) {
     res.status(error.statusCode || 500).json({
